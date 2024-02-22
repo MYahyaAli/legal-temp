fde1ea7ab460d6e66a07f2adfc5c02d315a3f40b
MYahya <myahya2919@gmail.com>
2024-02-23 01:03:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/NavBar";
 import Login from "./Components/LoginSignup/Login";
+import Signup from "./Components/LoginSignup/Signup";
 import './Components/LoginSignup/LoginSignup.css'
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
       <Router>
         <Navbar/>
           <Routes>
-            {/* <Route path="/" element={<Signup />} /> */}
             <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
       </Router>
     </div>
--- src/Components/LoginSignup/Login.jsx
+++ src/Components/LoginSignup/Login.jsx
@@ -1,30 +1,26 @@
-// import React, {useState, useEffect } from 'react';
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 function Login() {
-    // const[users, setUser] = useState([]);
-    // const navigate = useNavigate();
 
     return(
-        <div>
+        <div className='d-flex justify-content-center align-items-center '>
             <div className=" cnt container-sm">
-                <h1 className='mb-3 text-center'>Login</h1>
+                <h1 className='mb-3 text-center'><strong>Login</strong></h1>
                 <form action="" className="container-md">
                 <div class="mb-3">
-                    <label for="emailInput" class="form-label">Email <img src={email_icon} alt="email_icon" /></label>
-                    <input type="email" class="form-control" aria-describedby="emailHelp"/>
+                    <label htmlFor="email" class="form-label">Email <img src={email_icon} alt="email_icon" /></label>
+                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder='Enter Email'/>
                 </div>
                 <div class="mb-3">
-                    <label for="PasswordInput" class="form-label">Password <img src={password_icon} alt="password_icon" /></label>
-                    <input type="password" class="form-control" />
+                    <label htmlFor="PasswordInput" class="form-label">Password <img src={password_icon} alt="password_icon" /></label>
+                    <input type="password" class="form-control" placeholder='Password'/>
                 </div>
                 <div className="row justify-content-around">
-                    <button type="submit" class="btn btn-primary rounded-pill col-4">Login</button>
-                    <button type="submit" class="btn btn-primary rounded-pill col-4">SignUp</button>
+                    <button type="submit" class="btn btn-success rounded-pill col-4">Login</button>
+                    <Link to="/signup" class="btn btn-default rounded-pill col-4 border text-decoration-none">SignUp</Link>
                 </div>
                 </form>
             </div>
